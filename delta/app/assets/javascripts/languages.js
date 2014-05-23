d2db536b50236295fd308e8ca5d10ed06c262c66
plondon <plondon@gm.slc.edu>
2014-05-23 17:58:31 -0400

    first go at homepage, start adding colors

--- delta/app/assets/javascripts/languages.js
+++ delta/app/assets/javascripts/languages.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
+  // dynamically load end language drop down
   start_languages = $('#start_language').text().split("\n")
   end_languages   = $('#end_language').text().split("\n")
 
   $('#start_language').on('change', function(){
     start_language_selected = $('#start_language option:selected').text()
-    end_languages   = $('#start_language').text().split("\n")
+    end_languages           = $('#start_language').text().split("\n")
 
     end_languages.forEach(function(language) {
       if (start_language_selected === language) {
@@ -19,9 +20,20 @@ $(document).ready(function(){
       $('#end_language')
         .append("<option value='" + i + "'>" + end_languages[i] + "</option>")
     }
-    end_languages.forEach(function(language) {
-
-    });
-
   });
+  $('#end_language').on('change', function(){
+    end_language_selected = $('#end_language option:selected').text()
+  });
+
+  // $('#start_language').on('click', function(event) {
+  //   return false;
+  // })
+
+  // get request to the categories controller
+
+  // $('a').on('click', function(){
+  //   console.log(start_language_selected)
+  //   console.log(end_language_selected)
+  //   $.get('categories/show', {languages: {start_language: start_language_selected, end_language: end_language_selected}})
+  // });
 });
