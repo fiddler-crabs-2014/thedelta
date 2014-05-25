c40b4d15aef24ad25720211bea5d6af52b486709
Shaun McGeever <shaunmcgeever@gmail.com>
2014-05-25 00:08:29 -0400

    fix saving new answers and redirecting

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,16 +2,32 @@ require 'json'
 
 class AnswersController < ApplicationController
   def new
-    @answer = Answer.new
+    @question = Question.find(params[:question])
+    @answer = Answer.create(question_id: @question.id, user_id: current_user.id)
+    p "AT ANSWER"
+    p @answer
   end
 
   def create
-    @answer = Answer.create(delta: params[:delta].to_json)#,
-                        #user_id: params[:user_id], 
-                        #question_id: params[:question_id])
+    p "CURRENT USER:"
+    p current_user
+    p "CURRENT SESSION:"
+    p session
+    p params[:delta]
+    p "AT ANSWER"
+
+    ## This logic needs to be fixed.
+    @answer = Answer.last
+    if params[:delta]
+      @answer.update_attributes(delta: params[:delta].to_json)#,
+      render json: @answer.question_id
+    elsif JSON.parse(params[:delta].to_json) == ["0"]
+      @answer.destroy
+    end                   #user_id: current_user.id, 
+                        #question_id: params[:answer][:question_id])
 
     #if @answer.save
-      render json: @answer.delta
+      # render json: @answer.delta
     #else
     #  redirect_to new_answer_path
     #end
