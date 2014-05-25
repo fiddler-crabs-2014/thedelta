cf0d3a46c625a442b3526773fa2bc69d8da0a606
Shaun McGeever <shaunmcgeever@gmail.com>
2014-05-25 01:22:04 -0400

    fixed bugs with saving deltas. renders deltas on answers index. answers controller test failing

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,33 +4,24 @@ class AnswersController < ApplicationController
   def new
     @question = Question.find(params[:question])
     @answer = Answer.create(question_id: @question.id, user_id: current_user.id)
-    p "AT ANSWER"
-    p @answer
   end
 
   def create
-    p "CURRENT USER:"
-    p current_user
-    p "CURRENT SESSION:"
-    p session
-    p params[:delta]
-    p "AT ANSWER"
 
     ## This logic needs to be fixed.
+    ## Figure out how to validate saving an answer in one operation.
+    ## The user id and question id is being saved in #new but the delta is saved below
+    ## this is a problem with the "post" from the form:
+    ## without authentication, it clears the session.  
+      
     @answer = Answer.last
     if params[:delta]
       @answer.update_attributes(delta: params[:delta].to_json)#,
       render json: @answer.question_id
     elsif JSON.parse(params[:delta].to_json) == ["0"]
       @answer.destroy
-    end                   #user_id: current_user.id, 
-                        #question_id: params[:answer][:question_id])
+    end                  
 
-    #if @answer.save
-      # render json: @answer.delta
-    #else
-    #  redirect_to new_answer_path
-    #end
   end
 
   private
