171268fc3a3426f7a32cba6906b5eaedd7832b7e
plondon <plondon@gm.slc.edu>
2014-05-25 17:56:34 -0400

    add precompile code for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precompile += %w( *.css *.js )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
