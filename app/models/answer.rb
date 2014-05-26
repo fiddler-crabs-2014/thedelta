03736f2562d5d6ebc32f431ce444d66b1435f850
Shaun McGeever <shaunmcgeever@gmail.com>
2014-05-26 17:39:14 -0400

    readd validations to answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ActiveRecord::Base
   validates :question_id, presence: true
   validates :user_id, presence: true
-  # validates :delta, presence: true
+  validates :delta, presence: true
   
   belongs_to :question
   belongs_to :user
