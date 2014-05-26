2e21b3a6297e7a85d4c57e41e123e5063b753afc
Shaun McGeever <shaunmcgeever@gmail.com>
2014-05-26 17:38:33 -0400

    fix saving answer bug. no phantom answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,36 +4,29 @@ class AnswersController < ApplicationController
   def new
     if current_user && params[:question]
       @question = Question.find(params[:question])
-      @answer = Answer.create(question_id: @question.id, user_id: current_user.id)
+      @answer = Answer.new     
     else
       redirect_to root_path
     end
   end
 
   def create
-
-    ## This logic needs to be fixed.
-    ## Figure out how to validate saving an answer in one operation.
-    ## The user id and question id is being saved in #new but the delta is saved below
-    ## this is a problem with the "post" from the form:
-    ## without authentication, it clears the session.  
-      
-    @answer = Answer.last
     if !params[:delta].nil? && params[:delta].length >1
-      @answer.update_attributes(delta: params[:delta].to_json)#,
-      render json: { question_id: @answer.question_id }.to_json
+      @answer = Answer.new(delta: params[:delta].to_json, user_id: params[:user_id], question_id: params[:question_id])            
+      if @answer.valid?
+        @answer.save
+        render json: { question_id: @answer.question_id }.to_json
+      else
+        @answer.errors.full_messages
+      end
     else
-      @answer.destroy
-      render json: { question_id: @answer.question_id }.to_json
-    end                  
-
+      flash[:notice] = "That was not a valid Answer."
+      redirect_to new_answer_path(question: params[:question_id])
+    end
   end
 
   private
     def answer_params
-      params.require(:answer).permit(:delta,
-                                     :question_id,
-                                     :category_id,
-                                     :user_id)
+      params.require(:answer).permit(:delta, :question_id, :user_id)
     end
 end
