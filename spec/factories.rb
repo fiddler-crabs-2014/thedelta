7239c868002973369624a65f67b1b29b8748666f
Adrian Pask <adrian.pask@gmail.com>
2014-05-27 16:47:08 -0400

    Vote and admin tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -51,11 +51,17 @@ FactoryGirl.define do
     # end
   end
 
-  factory :answer do 
-    question_id 1
-    user_id 1
+  # factory :answer do 
+  #   question_id 1
+  #   user_id 1
+  #   delta "{\"0\":[\"\",\"0\"],\"1\":[\"t\",\"1423\"],\"2\":[\"th\",\"1511\"]}"
+  #   vote_count {rand(1..10)}
+  # end 
+
+  factory :answer do
+    association :question, factory: :question
+    association :user, factory: :user
     delta "{\"0\":[\"\",\"0\"],\"1\":[\"t\",\"1423\"],\"2\":[\"th\",\"1511\"]}"
-    vote_count {rand(1..10)}
   end
 
   factory :category do 
