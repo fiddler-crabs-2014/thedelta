38e3b86684fa62ecbc575f502514ae09673e8bc6
Shaun McGeever <shaunmcgeever@gmail.com>
2014-05-27 18:41:59 -0400

    deleted unnecessary line in factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -51,20 +51,9 @@ FactoryGirl.define do
     # end
   end
 
-<<<<<<< HEAD
-  # factory :answer do 
-  #   question_id 1
-  #   user_id 1
-  #   delta "{\"0\":[\"\",\"0\"],\"1\":[\"t\",\"1423\"],\"2\":[\"th\",\"1511\"]}"
-  #   vote_count {rand(1..10)}
-  # end 
 
   factory :answer do
-    association :question, factory: :question
-=======
-  factory :answer do
     association :question, factory: :question 
->>>>>>> master
     association :user, factory: :user
     delta "{\"0\":[\"\",\"0\"],\"1\":[\"t\",\"1423\"],\"2\":[\"th\",\"1511\"]}"
   end
