fd94053c8aea7803954911e3c883175d238daea3
plondon <plondon@gm.slc.edu>
2014-05-27 16:48:16 -0400

    style answer show page and add links to redirect back to appropriate location (from header)

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -10,7 +10,7 @@ class VotesController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       #format.xml  { render :xml => @post }
-      format.json {render json: {text: "#{@answer.upvote_count} Up / #{@answer.downvote_count} Down", id: "#vote-result-" + params[:answer_id] } }
+      format.json {render json: {text: "#{@answer.upvote_count} Up | #{@answer.downvote_count} Down", id: "#vote-result-" + params[:answer_id] } }
 
     end
 
