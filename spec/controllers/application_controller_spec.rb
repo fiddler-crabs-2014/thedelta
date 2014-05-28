03194cf24e196bd104b005e57582d0339ee171f8
Shaun McGeever <shaunmcgeever@gmail.com>
2014-05-28 01:11:16 -0400

    add application tests. testing for current_user with and with users, without users and admin rights

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,29 +2,178 @@ require 'spec_helper'
 
 describe ApplicationController do 
 
+  context "when a user is not signed in and no session is set" do
+  	before { session[:user_id] = nil }
+    before { controller.stub(current_user: nil) }
+    describe "#signed_in" do
+      it "should return false"do
+      controller.signed_in?.should be_false
+  	 end
+    end
 
-	describe "#new" do
-		context "with current_user" do
-			
-		end
-	end
+  	describe "#current_user" do
+  		it "should return nil" do
+  		  controller.current_user.should be_nil
+  		end
+  	end
+
+  	describe "#current_admin" do
+      it "should return nil" do
+        controller.current_admin.should be_nil
+      end
+  	end
+
+    describe "#current_user?" do
+      it "should return false" do
+        controller.current_user?("").should be_false
+      end
+    end
+
+    describe "#sign_out" do
+      xit "should return nil" do
+        controller.sign_out.to_not be_success
+      end
+    end
+
+    describe "#signed_in_user" do
+      xit "should redirect to login with flash message" do
+        controller.stub(signed_in?: false)
+        controller.signed_in_user
+        flash[:notice].should eq("Please sign in.")
+      end
+    end
 
 
-	describe "#show" do
+    describe "#confirm_logged_in" do
+      xit "should redirect to login with flash message" do
+        expect(controller.confirm_logged_in).to return_error
+      end
+    end
 
-		it "should find the answer given answer id" do
-		    get :show, id: answer.id
-		    expect(response).to be_success
-		end
-	end
+    describe "#confirm_admin" do
+      xit "should redirect to login with flash message" do
+        controller.confirm_admin
+        current_path.should == login_path
+      end
+    end
+  end
+
+  context "when a user is signed in and a session is set" do
+    let!(:user) { FactoryGirl.create(:user) }
+    before { controller.stub(current_user: user) }
+    before { session[:user_id] = user.id }
+    
+    describe "#signed_in" do
+      it "should return true"do
+        controller.signed_in?.should be_true
+      end
+    end
+
+    describe "#current_user" do
+      it "should return current user id" do
+        controller.current_user.should eq(user)
+      end
+    end
+
+    describe "#current_admin" do
+      it "should return nil" do
+        controller.current_admin.should be_nil
+      end
+    end
+
+    describe "#current_user?" do
+      it "should return true" do
+        controller.current_user?(user).should be_true
+      end
+    end
+
+    describe "#sign_out" do
+      xit "should return nil" do
+        controller.sign_out.should be_nil
+      end
+    end
+
+    describe "#signed_in_user" do
+      it "should return nil" do
+        controller.stub(signed_in?: true)
+        controller.signed_in_user.should be_nil
+      end
+    end
 
 
-	describe "#create" do
+    describe "#confirm_logged_in" do
+      it "should return true" do
+        controller.confirm_logged_in.should be_true
+      end
+    end
 
-		it "should respond with a delta paramter" do
-		    get :create, delta: answer.delta
-		    expect(response).to be_success
-		end
+    describe "#confirm_admin" do
+      xit "should redirect to login with flash message" do
+        controller.confirm_admin.should be_nil
+        # current_path.should == login_path
+      end
+    end
+  end
 
-	end
+  context "when a user is an admin" do
+    let!(:admin) { FactoryGirl.create(:admin) }
+    # admin.admin = true
+    # user = double("receiver", :admin => true)
+    # admin.should_receive(:admin).and_return(true)
+
+    before { controller.stub(current_user: admin) }
+    before { session[:user_id] = admin.id }
+    
+    describe "#signed_in" do
+      it "should return true"do
+        controller.signed_in?.should be_true
+      end
+    end
+
+    describe "#current_user" do
+      it "should return current user id" do
+        controller.current_user.should eq(admin)
+      end
+    end
+
+    describe "#current_admin" do
+      it "should return true" do
+        controller.current_admin.should be_true
+      end
+    end
+
+    describe "#current_user?" do
+      it "should return true" do
+        controller.current_user?(admin).should be_true
+      end
+    end
+
+    describe "#sign_out" do
+      xit "should return nil" do
+        controller.sign_out.should be_nil
+      end
+    end
+
+    describe "#signed_in_user" do
+      it "should return nil" do
+        controller.stub(signed_in?: true)
+        controller.signed_in_user.should be_nil
+      end
+    end
+
+
+    describe "#confirm_logged_in" do
+      it "should return true" do
+        controller.confirm_logged_in.should be_true
+      end
+    end
+
+    describe "#confirm_admin" do
+      it "should return true" do
+        controller.confirm_admin.should be_true
+        # current_path.should == login_path
+      end
+    end
+  end
+
 end
