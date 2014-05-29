3951f987b1a9e0640979594772c510422564f328
plondon <plondon@gm.slc.edu>
2014-05-28 21:43:08 -0400

    add features based on ux feedback

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,25 +1,28 @@
 class VotesController < ApplicationController
 
   def record
-
+    p params[:question_id]
     @answer = Answer.find(params[:answer_id])
     record_vote = Vote.record_vote(session[:user_id], 
       params[:answer_id], params[:vote_value])
 
-    
-    respond_to do |format|
-      format.html { redirect_to answer_path(@answer) }
-      #format.xml  { render :xml => @post }
-      format.json {render json: {text: "#{@answer.upvote_count} Up | #{@answer.downvote_count} Down", id: "#vote-result-" + params[:answer_id] } }
-
+    if params[:question_id]
+      respond_to do |format|
+        format.html { redirect_to question_path(params[:question_id]) }
+        format.json {render json: {text: "#{@answer.upvote_count} Up | #{@answer.downvote_count} Down", id: "#vote-result-" + params[:answer_id] } }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to answer_path(@answer) }
+        format.json {render json: {text: "#{@answer.upvote_count} Up | #{@answer.downvote_count} Down", id: "#vote-result-" + params[:answer_id] } }
+      end
     end
-
   end
 
   private
   
   def user_params
-     params.require(:vote).permit(:user_id, :answer_id, :vote_value)
+     params.require(:vote).permit(:user_id, :answer_id, :vote_value, :question_id)
   end
 
 end
