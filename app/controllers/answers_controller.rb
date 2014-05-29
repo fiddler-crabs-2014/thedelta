7125b27bb98116e5530b01d75600c4e22eaa9a6f
Shaun McGeever <shaunmcgeever@gmail.com>
2014-05-29 04:18:43 -0400

    refactored answers and admin answers controllers. created BaseAnswerController for similar methods

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,38 +1,15 @@
 require 'json'
 
-class AnswersController < ApplicationController
+class AnswersController < BaseAnswerController
+  before_action :current_user, except: [:show]
 
   def new
-    if current_user && params[:question]
+    if params[:question]
       @question = Question.find(params[:question])
       @answer = Answer.new     
     else
       redirect_to login_path
     end
   end
-
-  def show
-    @answer = Answer.find(params[:id])
-  end
-
-  def create
-    if params[:delta].present? && params[:delta].length >1
-      @answer = Answer.new(delta: params[:delta].to_json, user_id: current_user.id, question_id: params[:question_id]) 
-      if @answer.save
-        render json: { question_id: @answer.question_id }.to_json
-      else
-        @answer.errors.full_messages
-        redirect_to new_answer_path
-      end
-    else
-      flash[:notice] = "That was not a valid Answer."
-      redirect_to new_answer_path(question: params[:question_id])
-    end
-  end
-
-  private
-    def answer_params
-      params.require(:answer).permit(:delta, :question_id, :user_id)
-    end
-
+  
 end
